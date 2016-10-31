8535fb24809334c42ce876872812887b935ee83c
Kemyke <kemyyy@gmail.com>
2016-10-31 10:15:24 +0100

    Update entrypoint.sh
    use Agent.Listener command line parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-expect -f installagent.exp
+/bin/Agent.Listener configure --acceptteeeula --url $TFS_URL --auth Negotiate --username $TFS_USERNAME --password $TFS_PWD --pool $TFS_AGENTPOOL --agent $TFS_AGENTNAME --work _work
 
-exec bin/Agent.Listener
+exec /bin/Agent.Listener
